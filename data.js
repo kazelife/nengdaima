05709c8fa4ac12c2088764704c34a6eddbc40234
kazelife <kazelife@outlook.com>
2025-01-02 13:09:44 +0800

    1

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
 const data = [
-  { title: "充电宝", content: "小于等于27000毫安的能。但不要在机上使用。" },
-  { title: "水", content: "不能。液体都不能。" },
+  { title: "充电宝", content: "能。但需要小于等于27000毫安。注意不要在飞机上使用。" },
+  { title: "水", content: "不能。但飞机上提供免费的水和杯子。" },
+  { title: "饮料", content: "不能。但飞机上提供免费的水和杯子。" },
   { title: "自热锅", content: "不能。自热食品都不能。" },
+  { title: "自热米饭", content: "不能。自热食品都不能。" },
   { title: "花露水", content: "不能。含酒精都不能。" },
   { title: "风油精", content: "不能。含酒精都不能。" },
   { title: "化妆品", content: "能。但需要小于等于100毫升。" },
@@ -14,11 +16,11 @@ const data = [
   { title: "狗", content: "不能。" },
   { title: "猫", content: "不能。" },
   { title: "打火机", content: "不能。" },
-  { title: "笔记本电脑", content: "能。" },
-  { title: "平板", content: "能。" },
-  { title: "iPad", content: "能。" },
-  { title: "刀", content: "能。但刃长不能超过16厘米。" },
-  { title: "沐浴露", content: "高性能 MacBook Pro 笔记本电脑" }
+  { title: "笔记本电脑", content: "能。但电子产品需要拿出来过安检。" },
+  { title: "平板", content: "能。但电子产品需要拿出来过安检。" },
+  { title: "iPad", content: "能。但电子产品需要拿出来过安检。" },
+  { title: "刀", content: "能。但刃长不能超过6厘米。" },
+  { title: "沐浴露", content: "能。但需要小于等于100克。" }
 ];
 
 export default data;
