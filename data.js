8309a1a57539682239250597cbeab13dd6aa4155
kazelife <kazelife@outlook.com>
2025-01-02 11:56:17 +0800

    测试

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  { title: "充电宝", content: "小于等于27000毫安的能。但不要在机上使用。" },
+  { title: "水", content: "不能。液体都不能。" },
+  { title: "自热锅", content: "不能。自热食品都不能。" },
+  { title: "花露水", content: "不能。含酒精都不能。" },
+  { title: "风油精", content: "不能。含酒精都不能。" },
+  { title: "化妆品", content: "能。但需要小于等于100毫升。" },
+  { title: "爽肤水", content: "能。但需要小于等于100毫升。" },
+  { title: "牙膏", content: "能。但需要小于等于100克。" },
+  { title: "洗面奶", content: "能。但需要小于等于100克。" },
+  { title: "防晒霜", content: "能。但需要小于等于100克。" },
+  { title: "榴莲", content: "不能。" },
+  { title: "臭豆腐", content: "不能。" },
+  { title: "狗", content: "不能。" },
+  { title: "猫", content: "不能。" },
+  { title: "打火机", content: "不能。" },
+  { title: "笔记本电脑", content: "能。" },
+  { title: "平板", content: "能。" },
+  { title: "iPad", content: "能。" },
+  { title: "刀", content: "能。但刃长不能超过16厘米。" },
+  { title: "沐浴露", content: "高性能 MacBook Pro 笔记本电脑" }
+];
+
+export default data;
